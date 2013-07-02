ba9cd674597c934bf271f7eb778df1bf550bf226
Dan Tao <daniel.tao@gmail.com>
2013-07-02 09:01:55 -0700

    updated page titles and refactored idea.js a little

--- idea.js
+++ idea.js
@@ -5,6 +5,11 @@ function isMarkdownFile(file) {
 function displayIdea(response) {
   var html = markdown.toHTML(response);
   document.getElementById('idea').innerHTML = html;
+
+  var headings = document.getElementsByTagName('h1');
+  if (headings.length > 0) {
+    document.title = 'Dan Tao - ' + headings[0].textContent;
+  }
 }
 
 function makeAjaxRequest(url, callback) {
@@ -21,11 +26,23 @@ function makeAjaxRequest(url, callback) {
   request.send();
 }
 
-window.addEventListener('load', function() {
+function loadIdeaForLocation() {
   var ideaName = window.location.pathname.match(/ideas\/(.*)$/);
+
+  if (!ideaName) {
+    ideaName = window.location.search.match(/\bidea=([^&]*)/);
+  }
+
   if (ideaName) {
     makeAjaxRequest('/ideas/' + ideaName[1] + '.md', displayIdea);
   }
+}
 
+function displayPathInFooter() {
   document.getElementById('path').textContent = window.location.pathname;
+}
+
+window.addEventListener('load', function() {
+  loadIdeaForLocation();
+  displayPathInFooter();
 });
