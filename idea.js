1dd81822dd2a09afb3be965466ed948389e2507a
Dan Tao <daniel.tao@gmail.com>
2013-07-10 14:02:25 -0700

    wrote about Up/Down idea

--- idea.js
+++ idea.js
@@ -19,9 +19,13 @@ function displayIdea(response) {
   }
 
   var html = marked(response);
-  document.getElementById('idea').innerHTML = html;
+  var article = document.createElement('article');
+  article.id = 'idea';
+  article.innerHTML = html;
 
-  var headings = document.getElementsByTagName('h1');
+  document.getElementById('content').appendChild(article);
+
+  var headings = article.querySelector('h1');
   if (headings.length > 0) {
     document.title = 'Dan Tao - ' + headings[0].textContent;
   }
@@ -65,7 +69,99 @@ function displayPathInFooter() {
   document.getElementById('path').textContent = window.location.pathname;
 }
 
+function pluralizeCount(count, word) {
+  if (count === 1) {
+    return count + ' ' + word;
+  } else {
+    return count + ' ' + word + 's';
+  }
+}
+
+function addClass(element, className) {
+  if (element.classList) {
+    element.classList.add(className);
+  } else {
+    var classes = element.className.split(/\s+/);
+    classes.push(className);
+    element.className = classes.join(' ');
+  }
+}
+
+function removeClass(element, className) {
+  if (element.classList) {
+    element.classList.remove(className);
+  } else {
+    var classes = element.className.split(/\s+/);
+    removeFromArray(classes, className);
+    element.className = classes.join(' ');
+  }
+}
+
+function removeFromArray(array, element) {
+  for (var i = array.length - 1; i >= 0; --i) {
+    if (array[i] === element) {
+      array.splice(i, 1);
+    }
+  }
+}
+
+function displayVoteCounts(counts) {
+  document.getElementById('upvote-count').textContent = pluralizeCount(counts.upvotes, 'upvote');
+  document.getElementById('downvote-count').textContent = pluralizeCount(counts.downvotes, 'downvote');
+}
+
+function handleUpDownResponse(response) {
+  if (response.status === 'duplicate') {
+    alert('You voted for this idea already.');
+    return false;
+  }
+  displayVoteCounts(response);
+  return true;
+}
+
+function getCurrentUrl() {
+  return window.location.hostname + window.location.pathname;
+}
+
+function initVotingButtons() {
+  UpDown.getCounts(displayVoteCounts);
+
+  var voteUpButton = document.getElementById('vote-up');
+  var voteDownButton = document.getElementById('vote-down');
+
+  voteUpButton.addEventListener('click', function() {
+    UpDown.voteUp(function(response) {
+      if (handleUpDownResponse(response)) {
+        addClass(voteUpButton, 'voted');
+        removeClass(voteDownButton, 'voted');
+        localStorage[getCurrentUrl()] = 'up';
+      }
+    });
+  });
+
+  voteDownButton.addEventListener('click', function() {
+    UpDown.voteDown(function(response) {
+      if (handleUpDownResponse(response)) {
+        addClass(voteDownButton, 'voted');
+        removeClass(voteUpButton, 'voted');
+        localStorage[getCurrentUrl()] = 'down';
+      }
+    });
+  });
+
+  switch (localStorage[getCurrentUrl()]) {
+    case 'up':
+      addClass(voteUpButton, 'voted');
+      break;
+
+    case 'down':
+      addClass(voteDownButton, 'voted');
+      break;
+  }
+}
+
 window.addEventListener('load', function() {
   loadIdeaForLocation();
   displayPathInFooter();
+  initVotingButtons();
 });
