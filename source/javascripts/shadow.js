59687e5480ba1dbcee853159b279d6d7fc97c70c
Dan Tao <daniel.tao@gmail.com>
2013-10-02 09:49:00 -0700

    working on this shadow idea

---
+++ source/javascripts/shadow.js
@@ -0,0 +1,25 @@
+function getRule(selector) {
+  var rules = document.styleSheets[0].cssRules;
+  for (var i = 0; i < rules.length; ++i) {
+    if (rules[i].selectorText === selector) {
+      return rules[i];
+    }
+  }
+  return null;
+}
+
+function setBoxShadow() {
+  var screenHeight = window.innerHeight,
+      screenWidth  = window.innerWidth,
+      size         = Math.min(screenHeight, screenWidth);
+
+  var shadowRule = getRule('#shadow');
+  shadowRule.style.boxShadow = 'inset 0 0 ' + (size * 2) + 'px ' + (size / 2) + 'px #000';
+
+  var hoverShadowRule = getRule('nav a:hover ~ #shadow');
+  hoverShadowRule.style.boxShadow = 'inset 0 0 ' + size + 'px ' + (size / 3) + 'px #000';
+}
+
+setBoxShadow();
+
+window.addEventListener('resize', setBoxShadow);
